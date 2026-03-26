16e97f210d537d3520c81c840c27549c07022238
Ankush-0g <ankushgupta4747@gmail.com>
2026-03-26 21:35:19 +0530

    Change MongoDB connection to SRV format
    Updated MongoDB connection string to use SRV format.

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://ankushgupta4747_db_user:m7VD2QjSzpReH1kW@ankush7.o7ginho.mongodb.net/smart_queue_db?retryWrites=true&w=majority&appName=Ankush7")
 db = client["smart_queue_db"]
